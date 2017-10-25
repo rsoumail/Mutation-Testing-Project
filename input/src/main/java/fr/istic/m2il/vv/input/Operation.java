8eb560de4ede77f45ab6716a20716f1bbb0e506c
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-25 09:18:28 +0200

    Ajout de l'interface Operation

---
+++ input/src/main/java/fr/istic/m2il/vv/input/Operation.java
@@ -0,0 +1,8 @@
+package fr.istic.m2il.vv.input;
+
+public interface Operation {
+    Double getFisrtMember();
+    void setFirstMember();
+    Double getSecondMember();
+    void setSecondMember();
+}
