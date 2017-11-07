c481025110ceff419adaae40f55cf53f3b8c89b7
Soumaila Ramadan <ramadansum@gmail.com>
2017-11-07 14:59:18 +0100

    Ajout de dépendence et initialisation du javaassit

--- mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
@@ -1,4 +1,30 @@
 package fr.istic.m2il.vv.mutation;
 
+import javassist.*;
+
 public class App {
+    public void main(String[] args){
+        try{
+            ClassPool pool = ClassPool.getDefault();
+            Loader loader = new Loader(pool);
+
+            Translator logger = new Translator() {
+                public void start(ClassPool classPool) throws NotFoundException, CannotCompileException {
+                    System.out.println("Starting...");
+                }
+
+                public void onLoad(ClassPool classPool, String classname) throws NotFoundException, CannotCompileException {
+                    System.out.println("Loading...: " + classname);
+                }
+            };
+
+            loader.addTranslator(pool, logger);
+            pool.appendClassPath("input/target/classes");
+        }
+        catch(Throwable exc) {
+            System.out.println("Oh, no! Something went wrong.");
+            System.out.println(exc.getMessage());
+            exc.printStackTrace();
+        }
+    }
 }
