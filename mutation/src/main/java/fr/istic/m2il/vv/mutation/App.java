333b5839742817a574aece49195655f7003e73bc
Waberi <waberi.houssein@gmail.com>
2017-11-14 21:26:50 +0100

    Ajout de dépendence et initialisation du javaassit

--- mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
@@ -1,9 +1,13 @@
 package fr.istic.m2il.vv.mutation;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
 import javassist.*;
 
 public class App {
-    public void main(String[] args){
+    public static void main(String[] args){
         try{
             ClassPool pool = ClassPool.getDefault();
             Loader loader = new Loader(pool);
@@ -19,7 +23,22 @@ public class App {
             };
 
             loader.addTranslator(pool, logger);
-            pool.appendClassPath("input/target/classes");
+            pool.appendClassPath("../input/target/classes");
+            pool.appendClassPath("../input/target/test-classes");
+            loader.run("fr.istic.m2il.vv.input.App",args);
+            
+            JUnitCore jUnitCore = new JUnitCore();
+            
+            String classes[] = {"fr.istic.m2il.vv.input.AdditionTest",
+            					"fr.istic.m2il.vv.input.DivisionTest",
+            					"fr.istic.m2il.vv.input.MultiplicationTest",
+            					"fr.istic.m2il.vv.input.SubstractionTest"};
+            for(CtClass ctClass : pool.get(classes)) {
+            	System.out.println("test: " + ctClass.getName());
+            	Request request = Request.aClass(ctClass.toClass());
+            	Result r = jUnitCore.run(request);
+            	System.out.println("Tests ran : " + r.getRunCount() + ", failed : " + r.getFailureCount());
+            }
         }
         catch(Throwable exc) {
             System.out.println("Oh, no! Something went wrong.");
