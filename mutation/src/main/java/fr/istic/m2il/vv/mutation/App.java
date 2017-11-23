55edb15c2a7f10a142c44053abdc09311e233dee
Waberi <waberi.houssein@gmail.com>
2017-11-23 21:10:38 +0100

    Mise à jour

--- mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/App.java
@@ -22,272 +22,261 @@ import static java.lang.System.in;
 
 public class App {
 
+	public static void main(String[] args) {
+		String inputPath, inputTestPath;
+		if (args.length < 1) {
+			System.err.println(args[0]);
+			System.err.println("Veuillez indiquer le chemin du projet source");
+			exit(0);
+		} else {
+			inputPath = args[0] + "/target/classes/";
+			inputTestPath = args[0] + "/target/test-classes/";
+			mutate(inputPath);
+			try {
+				runTest(inputPath, inputTestPath);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			deleteTarget(inputPath);
+		}
 
-    public static void main(String[] args){
-        String inputPath, inputTestPath;
-        if(args.length < 1){
-            System.err.println(args[0]);
-            System.err.println("Veuillez indiquer le chemin du projet source");
-         exit(0);
-        }
-        else {
-            inputPath = args[0] + "/target/classes/";
-            inputTestPath = args[0] + "/target/test-classes/";
-            mutate(inputPath);
-            try {
-                runTest(inputPath,
-                        inputTestPath);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            deleteTarget(inputPath);
-        }
+	}
 
-    }
+	public static void runTest(String targetClasses, String targetTestClasses)
+			throws ClassNotFoundException, MalformedURLException {
 
-    public static void runTest(String targetClasses, String targetTestClasses ) throws ClassNotFoundException, MalformedURLException{
+		URLClassLoader urlClassLoader = URLClassLoader
+				.newInstance(new URL[] { new URL("file://" + targetClasses), new URL("file://" + targetTestClasses), });
 
+		JUnitCore core = new JUnitCore();
+		Class<?>[] classes = { urlClassLoader.loadClass("fr.istic.m2il.vv.input.AdditionTest"),
+				urlClassLoader.loadClass("fr.istic.m2il.vv.input.SubstractionTest"),
+				urlClassLoader.loadClass("fr.istic.m2il.vv.input.MultiplicationTest"),
+				urlClassLoader.loadClass("fr.istic.m2il.vv.input.DivisionTest") };
+		for (Class<?> clazz : classes) {
+			System.out.println("Test: " + clazz.getName());
+			Request request = Request.aClass(clazz);
+			Result r = core.run(request);
 
+			System.out.println("For " + clazz.getName() + String.format("| RUN: %d", r.getRunCount()));
+			if (r.wasSuccessful())
+				System.out.println("For " + clazz.getName() + "| ALL SUCCEEDED !");
+			else
+				System.out.println("For " + clazz.getName() + "| FAILURE ! ");
+			for (Failure failure : r.getFailures()) {
+				System.out.println(failure.toString());
+				System.out.println(failure.getTrace());
+			}
 
+			System.out.println("For " + clazz.getName() + String.format("| TIME: %dms", r.getRunTime()));
+			System.out.println("");
+		}
 
-        URLClassLoader urlClassLoader = URLClassLoader.newInstance(new URL[]{
-                new URL("file://" + targetClasses),
-                new URL("file://" + targetTestClasses),
-        });
+		System.out.println("ALL TESTS FINISHED");
 
-        JUnitCore core = new JUnitCore();
-        Class<?>[] classes = {
-                urlClassLoader.loadClass("fr.istic.m2il.vv.input.AdditionTest"),
-                urlClassLoader.loadClass("fr.istic.m2il.vv.input.SubstractionTest"),
-                urlClassLoader.loadClass("fr.istic.m2il.vv.input.MultiplicationTest"),
-                urlClassLoader.loadClass("fr.istic.m2il.vv.input.DivisionTest")
-                };
-        for(Class<?> clazz : classes) {
-            	System.out.println("Test: " + clazz.getName());
-            	Request request = Request.aClass(clazz);
-            	Result r = core.run(request);
+	}
 
-                System.out.println("For " +  clazz.getName() + String.format("| RUN: %d", r.getRunCount()));
-                if(r.wasSuccessful())
-                    System.out.println( "For " +  clazz.getName() + "| ALL SUCCEEDED !");
-                else
-                    System.out.println( "For " +  clazz.getName() + "| FAILURE ! ");
-                for (Failure failure : r.getFailures()){
-                    System.out.println(failure.toString());
-                    System.out.println(failure.getTrace());
-                }
+	public static void deleteTarget(String inputPath) {
+		File target = new File(inputPath);
+		for (String file : target.list()) {
+			File currentFile = new File(target.getPath(), file);
+			currentFile.delete();
+		}
+	}
 
-            System.out.println("For " +  clazz.getName() + String.format("| TIME: %dms", r.getRunTime()));
-            System.out.println("");
-        }
+	public static void buildTarget() {
 
-        System.out.println("ALL TESTS FINISHED");
+	}
 
-    }
+	public static void mutate(String inputPath) {
+		try {
+			ClassPool pool = ClassPool.getDefault();
+			Loader loader = new Loader(pool);
 
-    public static void deleteTarget(String inputPath){
-        File target = new File(inputPath);
-        for(String file : target.list()){
-            File currentFile = new File(target.getPath(),file);
-            currentFile.delete();
-        }
-    }
+			Translator logger = new Translator() {
+				public void start(ClassPool classPool) throws NotFoundException, CannotCompileException {
+					System.out.println("Starting...");
+				}
 
-    public static void buildTarget(){
+				public void onLoad(ClassPool classPool, String classname)
+						throws NotFoundException, CannotCompileException {
+					System.out.println("Loading...: " + classname);
+				}
+			};
 
-    }
+			loader.addTranslator(pool, logger);
+			pool.appendClassPath(inputPath);
 
-    public static void mutate(String inputPath){
-        try{
-            ClassPool pool = ClassPool.getDefault();
-            Loader loader = new Loader(pool);
+			/* LoadClasses To mutate */
+			String classes[] = { "fr.istic.m2il.vv.input.Operation", "fr.istic.m2il.vv.input.Addition",
+					"fr.istic.m2il.vv.input.Division", "fr.istic.m2il.vv.input.Multiplication",
+					"fr.istic.m2il.vv.input.Substraction" };
 
-            Translator logger = new Translator() {
-                public void start(ClassPool classPool) throws NotFoundException, CannotCompileException {
-                    System.out.println("Starting...");
-                }
+			mutateArithmeticOperation(pool, classes, inputPath);
+			mutateComparisonOperation(pool, classes, inputPath);
 
-                public void onLoad(ClassPool classPool, String classname) throws NotFoundException, CannotCompileException {
-                    System.out.println("Loading...: " + classname);
-                }
-            };
+		} catch (Throwable exc) {
+			System.out.println("Impossible de charger les sources de l'input.");
+			System.out.println(exc.getMessage());
+			exc.printStackTrace();
+		}
+	}
 
-            loader.addTranslator(pool, logger);
-            pool.appendClassPath(inputPath);
+	public static void mutateArithmeticOperation(ClassPool pool, String[] classes, String inputPath)
+			throws NotFoundException, CannotCompileException, IOException, BadBytecode {
 
-            /*LoadClasses To mutate*/
-            String classes[] = {
-                    "fr.istic.m2il.vv.input.Operation",
-                    "fr.istic.m2il.vv.input.Addition",
-                    "fr.istic.m2il.vv.input.Division",
-                    "fr.istic.m2il.vv.input.Multiplication",
-                    "fr.istic.m2il.vv.input.Substraction"
-            };
+		for (CtClass ctClass : pool.get(classes)) {
+			ctClass.defrost();
+			CtMethod methode = ctClass.getDeclaredMethod("operate");
 
-            mutateArithmeticOperation(pool, classes, inputPath);
-            mutateComparisonOperation(pool, classes, inputPath);
+			if (ctClass.getName().contains("Addition")) {
 
-        }
-        catch(Throwable exc) {
-            System.out.println("Impossible de charger les sources de l'input.");
-            System.out.println(exc.getMessage());
-            exc.printStackTrace();
-        }
-    }
+				int oldopcode = 0;
+				int newOpcode = 0;
 
-    public static void mutateArithmeticOperation(ClassPool pool, String[] classes, String inputPath) throws NotFoundException, CannotCompileException, IOException, BadBytecode {
+				switch (methode.getReturnType().getName().toString()) {
+				case "double":
+					oldopcode = Opcode.DADD;
+					newOpcode = Opcode.DSUB;
+					break;
 
-        for(CtClass ctClass: pool.get(classes)){
-            ctClass.defrost();
-            CtMethod methode = ctClass.getDeclaredMethod("operate");
+				case "int":
+					oldopcode = Opcode.IADD;
+					newOpcode = Opcode.ISUB;
+					break;
 
-            if(ctClass.getName().contains("Addition")){
+				case "float":
+					oldopcode = Opcode.FADD;
+					newOpcode = Opcode.FSUB;
+					break;
 
-                int oldopcode = 0;
-                int newOpcode = 0;
+				case "long":
+					oldopcode = Opcode.LADD;
+					newOpcode = Opcode.LSUB;
+					break;
 
-                switch (methode.getReturnType().getName().toString()){
-                    case "double":
-                        oldopcode = Opcode.DADD;
-                        newOpcode = Opcode.DSUB;
-                        break;
+				}
+				BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
+				bcArithmeticEditor.replace(methode);
+			}
+			if (ctClass.getName().contains("Substraction")) {
 
-                    case "int":
-                        oldopcode = Opcode.IADD;
-                        newOpcode = Opcode.ISUB;
-                        break;
+				int oldopcode = 0;
+				int newOpcode = 0;
 
-                    case "float":
-                        oldopcode = Opcode.FADD;
-                        newOpcode = Opcode.FSUB;
-                        break;
+				switch (methode.getReturnType().getName().toString()) {
+				case "double":
+					oldopcode = Opcode.DSUB;
+					newOpcode = Opcode.DADD;
+					break;
 
-                    case "long":
-                        oldopcode = Opcode.LADD;
-                        newOpcode = Opcode.LSUB;
-                        break;
+				case "int":
+					oldopcode = Opcode.ISUB;
+					newOpcode = Opcode.IADD;
+					break;
 
-                }
-                BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
-                bcArithmeticEditor.replace(methode);
-            }
-            if(ctClass.getName().contains("Substraction")){
+				case "float":
+					oldopcode = Opcode.FSUB;
+					newOpcode = Opcode.FADD;
+					break;
 
-                int oldopcode = 0;
-                int newOpcode = 0;
+				case "long":
+					oldopcode = Opcode.LSUB;
+					newOpcode = Opcode.LADD;
+					break;
 
-                switch (methode.getReturnType().getName().toString()){
-                    case "double":
-                        oldopcode = Opcode.DSUB;
-                        newOpcode = Opcode.DADD;
-                        break;
+				}
 
-                    case "int":
-                        oldopcode = Opcode.ISUB;
-                        newOpcode = Opcode.IADD;
-                        break;
+				BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
+				bcArithmeticEditor.replace(methode);
+			}
+			if (ctClass.getName().contains("Multiplication")) {
 
-                    case "float":
-                        oldopcode = Opcode.FSUB;
-                        newOpcode = Opcode.FADD;
-                        break;
+				int oldopcode = 0;
+				int newOpcode = 0;
 
-                    case "long":
-                        oldopcode = Opcode.LSUB;
-                        newOpcode = Opcode.LADD;
-                        break;
+				switch (methode.getReturnType().getName().toString()) {
+				case "double":
+					oldopcode = Opcode.DMUL;
+					newOpcode = Opcode.DDIV;
+					break;
 
-                }
+				case "int":
+					oldopcode = Opcode.IMUL;
+					newOpcode = Opcode.IDIV;
+					break;
 
-                BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
-                bcArithmeticEditor.replace(methode);
-            }
-            if(ctClass.getName().contains("Multiplication")){
+				case "float":
+					oldopcode = Opcode.FMUL;
+					newOpcode = Opcode.FDIV;
+					break;
 
-                int oldopcode = 0;
-                int newOpcode = 0;
+				case "long":
+					oldopcode = Opcode.LMUL;
+					newOpcode = Opcode.LDIV;
+					break;
 
-                switch (methode.getReturnType().getName().toString()){
-                    case "double":
-                        oldopcode = Opcode.DMUL;
-                        newOpcode = Opcode.DDIV;
-                        break;
+				}
 
-                    case "int":
-                        oldopcode = Opcode.IMUL;
-                        newOpcode = Opcode.IDIV;
-                        break;
+				BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
+				bcArithmeticEditor.replace(methode);
+			}
+			if (ctClass.getName().contains("Division")) {
 
-                    case "float":
-                        oldopcode = Opcode.FMUL;
-                        newOpcode = Opcode.FDIV;
-                        break;
+				int oldopcode = 0;
+				int newOpcode = 0;
 
-                    case "long":
-                        oldopcode = Opcode.LMUL;
-                        newOpcode = Opcode.LDIV;
-                        break;
+				switch (methode.getReturnType().getName().toString()) {
+				case "double":
+					oldopcode = Opcode.DDIV;
+					newOpcode = Opcode.DMUL;
+					break;
 
-                }
+				case "int":
+					oldopcode = Opcode.IDIV;
+					newOpcode = Opcode.IMUL;
+					break;
 
-                BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
-                bcArithmeticEditor.replace(methode);
-            }
-            if(ctClass.getName().contains("Division")){
+				case "float":
+					oldopcode = Opcode.FDIV;
+					newOpcode = Opcode.FMUL;
+					break;
 
-                int oldopcode = 0;
-                int newOpcode = 0;
+				case "long":
+					oldopcode = Opcode.LDIV;
+					newOpcode = Opcode.LMUL;
+					break;
 
-                switch (methode.getReturnType().getName().toString()){
-                    case "double":
-                        oldopcode = Opcode.DDIV;
-                        newOpcode = Opcode.DMUL;
-                        break;
+				}
 
-                    case "int":
-                        oldopcode = Opcode.IDIV;
-                        newOpcode = Opcode.IMUL;
-                        break;
+				BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
+				bcArithmeticEditor.replace(methode);
+			}
+		}
 
-                    case "float":
-                        oldopcode = Opcode.FDIV;
-                        newOpcode = Opcode.FMUL;
-                        break;
+	}
 
-                    case "long":
-                        oldopcode = Opcode.LDIV;
-                        newOpcode = Opcode.LMUL;
-                        break;
+	public static void mutateComparisonOperation(ClassPool pool, String[] classes, String inputPath)
+			throws NotFoundException, CannotCompileException, IOException, BadBytecode {
 
-                }
+		for (CtClass ctClass : pool.get(classes)) {
+			for (CtMethod method : ctClass.getDeclaredMethods()) {
+				if (method.getReturnType().equals(CtClass.voidType)) {
+					VoidMethodEditor voidMethodEditor = new VoidMethodEditor(inputPath);
+					try {
+						voidMethodEditor.replace(method);
+					} catch (CannotCompileException e) {
+						e.printStackTrace();
+					} catch (BadBytecode badBytecode) {
+						badBytecode.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 
-                BCArithmeticEditor bcArithmeticEditor = new BCArithmeticEditor(inputPath, oldopcode, newOpcode);
-                bcArithmeticEditor.replace(methode);
-            }
-        }
-
-    }
-
-    public static void mutateComparisonOperation(ClassPool pool, String[] classes, String inputPath) throws NotFoundException, CannotCompileException, IOException, BadBytecode {
-
-        for(CtClass ctClass: pool.get(classes)){
-            for (CtMethod method : ctClass.getDeclaredMethods()) {
-                if (method.getReturnType().equals(CtClass.voidType)) {
-                    VoidMethodEditor voidMethodEditor = new VoidMethodEditor(inputPath);
-                    try {
-                        voidMethodEditor.replace(method);
-                    } catch (CannotCompileException e) {
-                        e.printStackTrace();
-                    } catch (BadBytecode badBytecode) {
-                        badBytecode.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-    }
+	}
 }
