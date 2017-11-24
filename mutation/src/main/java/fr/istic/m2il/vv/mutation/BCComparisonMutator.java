16f7c82de70ff98c9d87bbbcdbc4b7d0ccb7dec2
Waberi <waberi.houssein@gmail.com>
2017-11-24 15:08:27 +0100

    BCComparaisonMutator

--- mutation/src/main/java/fr/istic/m2il/vv/mutation/BCComparisonMutator.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/BCComparisonMutator.java
@@ -1,4 +1,44 @@
 package fr.istic.m2il.vv.mutation;
 
-public class BCComparisonMutator {
+import java.io.IOException;
+
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.MethodInfo;
+
+public class BCComparisonMutator implements Mutator {
+
+	private String input;
+	private int oldOpcode, newOpcode;
+
+	public BCComparisonMutator(String input, int oldOpcode, int newOpcode) {
+		this.input = input;
+		this.oldOpcode = oldOpcode;
+		this.newOpcode = newOpcode;
+	}
+
+	@Override
+	public void replace(CtMethod ctMethod) throws CannotCompileException, BadBytecode, IOException {
+
+		MethodInfo methodInfo = ctMethod.getMethodInfo();
+		CodeAttribute code = methodInfo.getCodeAttribute();
+		CodeIterator iterator = code.iterator();
+
+		while (iterator.hasNext()) {
+			int pos = iterator.next();
+			if (iterator.byteAt(pos) == this.oldOpcode) {
+				iterator.writeByte(this.newOpcode, pos);
+				write(ctMethod.getDeclaringClass());
+			}
+		}
+	}
+
+	public void write(CtClass ctClass) throws CannotCompileException, IOException {
+		ctClass.writeFile(input);
+	}
+
 }
