9b2dbf141db874e6d20b903ee53fb8a916244015
Soumaila Ramadan <ramadansum@gmail.com>
2017-11-27 19:59:37 +0100

    Restructuration du code, et implementation du mutator executor

--- mutation/src/main/java/fr/istic/m2il/vv/mutation/MutatorExecutor.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/MutatorExecutor.java
@@ -1,12 +1,29 @@
 package fr.istic.m2il.vv.mutation;
 
+import fr.istic.m2il.vv.mutation.mutator.ArithmeticOperatorMutator;
 import fr.istic.m2il.vv.mutation.mutator.Mutator;
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.bytecode.BadBytecode;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MutatorExecutor {
 
-    public void execute(Mutator mutator, File targetProject){
+    public void execute(Mutator mutator, File targetProject) throws CannotCompileException, BadBytecode, NotFoundException, IOException {
+        /*for(CtClass ctClass: pool.get(classes)){
+            ctClass.defrost();
+            CtMethod[] methods = ctClass.getDeclaredMethods();
 
+            for(CtMethod method: methods){
+                //ArithmeticOperatorMutator bcArithmeticEditor = new ArithmeticOperatorMutator(inputPath);
+                mutator.mutate(method);
+                //mutators.add(bcArithmeticEditor);
+            }
+
+        }*/
     }
 }
--- mutation/src/main/java/fr/istic/m2il/vv/mutation/mutator/ComparisonOperatorMutator.java
+++ mutation/src/main/java/fr/istic/m2il/vv/mutation/mutator/ComparisonOperatorMutator.java
@@ -1,4 +1,47 @@
 package fr.istic.m2il.vv.mutation.mutator;
 
-public class ComparisonOperatorMutator {
+import fr.istic.m2il.vv.mutation.Utils;
+import javassist.CannotCompileException;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.bytecode.BadBytecode;
+import javassist.bytecode.CodeAttribute;
+import javassist.bytecode.CodeIterator;
+import javassist.bytecode.MethodInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ComparisonOperatorMutator implements Mutator{
+
+
+    private File inputPath;
+    private CtMethod original;
+    private HashMap<Integer, Integer> bytes = new HashMap<>();
+
+    public ComparisonOperatorMutator(File inputPath) {
+        this.inputPath = inputPath;
+    }
+
+    @Override
+    public void mutate(CtMethod ctMethod) throws CannotCompileException, BadBytecode, IOException, NotFoundException {
+        MethodInfo methodInfo = ctMethod.getMethodInfo();
+        CodeAttribute code = methodInfo.getCodeAttribute();
+        CodeIterator iterator = code.iterator();
+
+        while (iterator.hasNext()) {
+            int pos = iterator.next();
+            switch (iterator.byteAt(pos)) {
+
+            }
+
+            Utils.write(ctMethod.getDeclaringClass(), this.inputPath);
+        }
+    }
+
+    @Override
+    public void revert() throws CannotCompileException, IOException, BadBytecode {
+
+    }
 }
