3f5651b1cf1f6bed4297ddea9092f5ee51a47e4a
Soumaila Ramadan <ramadansum@gmail.com>
2017-12-01 00:36:51 +0100

    Restructuration et remise à niveau

--- mutator/src/main/java/fr/istic/m2il/vv/mutator/MutatorApp.java
+++ mutator/src/main/java/fr/istic/m2il/vv/mutator/MutatorApp.java
@@ -4,25 +4,16 @@ import fr.istic.m2il.vv.mutator.common.ClassLoaderParser;
 import fr.istic.m2il.vv.mutator.common.JavaAssistHelper;
 import fr.istic.m2il.vv.mutator.mutant.*;
 import fr.istic.m2il.vv.mutator.targetproject.TargetProject;
-import javassist.bytecode.BadBytecode;
-import org.apache.maven.shared.invoker.MavenInvocationException;
+
 
 import javassist.*;
 
 import java.io.File;
-import java.io.IOException;
 
 import static java.lang.System.exit;
 
 public class MutatorApp {
 
-    static ClassPool pool;
-    static Loader loader;
-    static CustomTranslator translator;
-    static File inputDir ;
-    static File testInputDir ;
-    static File saveSourcesDir;
-    static String[] _sourcesClasses;
 
 
     public static void main(String[] args) throws Exception {
@@ -55,46 +46,8 @@ public class MutatorApp {
 
 
             /*PITRunner pitRunner = new PITRunner();
-            pitRunner.run();
-*/
+            pitRunner.run();*/
         }
-    }
-
-    public static void mutate(File inputDir) throws CannotCompileException, BadBytecode, NotFoundException, IOException, ClassNotFoundException, InterruptedException, MavenInvocationException {
-
-        mutateVoidReturnType(_sourcesClasses, inputDir);
-        mutateBooleanReturnType(_sourcesClasses, inputDir);
-
-    }
-
-
-    public static void mutateVoidReturnType(String[] classes, File inputPath) throws NotFoundException, CannotCompileException, IOException, BadBytecode, ClassNotFoundException, InterruptedException, MavenInvocationException {
-
-        ClassLoader classLoader = MutatorApp.class.getClassLoader();
-        File file = new File(classLoader.getResource("application.properties").getFile());
-        for(CtClass ctClass: pool.get(classes)){
-            CtMethod[] methods = ctClass.getDeclaredMethods();
-
-            for(CtMethod method: methods){
-                /*Mutator mutant = new VoidMethodMutator(inputPath, new File(Utils.loadPropertiesFile(file).getProperty("targetproject")));
-                mutant.mutate(method);*/
-            }
-
-        }
-    }
-
-    public static void mutateBooleanReturnType(String[] classes, File inputPath) throws NotFoundException, CannotCompileException, IOException, BadBytecode, InterruptedException, MavenInvocationException {
-
-        ClassLoader classLoader = MutatorApp.class.getClassLoader();
-        File file = new File(classLoader.getResource("application.properties").getFile());
-        for(CtClass ctClass: pool.get(classes)){
-            for(CtMethod method: ctClass.getDeclaredMethods()){
-//                Mutator mutant = new BooleanMethodMutator(inputPath, new File(Utils.loadPropertiesFile(file).getProperty("targetproject")));
-//                mutant.mutate(method);
-            }
-
-        }
-
     }
 
 }
