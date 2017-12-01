7cb3d2555e8d26237ffd661bc0e8bd87b1d63c55
Waberi <waberi.houssein@gmail.com>
2017-12-01 17:41:00 +0100

    Ajout du mutant comparaison MutateType

--- mutator/src/main/java/fr/istic/m2il/vv/mutator/mutant/MutantType.java
+++ mutator/src/main/java/fr/istic/m2il/vv/mutator/mutant/MutantType.java
@@ -3,5 +3,6 @@ package fr.istic.m2il.vv.mutator.mutant;
 public enum MutantType {
     ArithmeticOperatorMutator,
     VoidMethodMutator,
-    BooleanMethodMutator;
+    BooleanMethodMutator,
+    ComparisonOperatorMutator;
 }
