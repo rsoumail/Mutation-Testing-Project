6ea4a6743e9759530f9e84c0a7fe5913f522917b
Waberi <waberi.houssein@gmail.com>
2017-12-21 14:19:05 +0100

    Rectification de comparaison

--- mutator/src/main/java/fr/istic/m2il/vv/mutator/mutant/ComparisonOperatorMutator.java
+++ mutator/src/main/java/fr/istic/m2il/vv/mutator/mutant/ComparisonOperatorMutator.java
@@ -51,7 +51,8 @@ public class ComparisonOperatorMutator implements Mutator {
 			MethodInfo methodInfo = ctMethod.getMethodInfo();
 			CodeAttribute code = methodInfo.getCodeAttribute();
 			CodeIterator iterator = code.iterator();
-			MVNRunner testRunner = new MVNRunner(this.targetProject.getPom().getAbsolutePath(), "surefire:test", "-Dtest=" + this.targetProject.getTestClassNameOfClass(ctMethod.getDeclaringClass().getName()));
+			MVNRunner testRunner = new MVNRunner(this.targetProject.getPom().getAbsolutePath(), "surefire:test",
+					"-Dtest=" + this.targetProject.getTestClassNameOfClass(ctMethod.getDeclaringClass().getName()));
 
 			while (iterator.hasNext()) {
 				HashMap<Integer, Integer> m = new HashMap<>();
@@ -59,19 +60,19 @@ public class ComparisonOperatorMutator implements Mutator {
 				switch (iterator.byteAt(pos)) {
 				// Replace operator < by <=
 				case Opcode.IF_ICMPLT:
-					//m.put(pos, Opcode.IF_ICMPGE);
+					// m.put(pos, Opcode.IF_ICMPGE);
 					m.put(pos, Opcode.IF_ICMPLE);
 					break;
 
 				// Replace operator > by >=
 				case Opcode.IF_ICMPGT:
-					//m.put(pos, Opcode.IF_ICMPLE);
+					// m.put(pos, Opcode.IF_ICMPLE);
 					m.put(pos, Opcode.IF_ICMPGE);
 					break;
 
 				// Replace operator <= by <
 				case Opcode.IF_ICMPLE:
-					//m.put(pos, Opcode.IF_ICMPGT);
+					// m.put(pos, Opcode.IF_ICMPGT);
 					m.put(pos, Opcode.IF_ICMPLT);
 					break;
 
@@ -79,51 +80,31 @@ public class ComparisonOperatorMutator implements Mutator {
 				case Opcode.IF_ICMPGE:
 					m.put(pos, Opcode.IF_ICMPGT);
 					break;
-
-//				//Replace operator > to >=
-//				case Opcode.IFGT:
-//					m.put(pos, Opcode.IFLE);
-//					break;
-//
-//				// < to >=
-//				case Opcode.IFLT:
-//					m.put(pos, Opcode.IFGE);
-//					break;
-//				
-//				// >= to <
-//				case Opcode.IFGE:
-//					m.put(pos, Opcode.IFLT);
-//					break;
-//
-//				// <= to >
-//				case Opcode.IFLE:
-//					m.put(pos, Opcode.IFGT);
-//					break;
 				}
-				if(!m.isEmpty()){
-                    iterator.writeByte(m.get(pos), pos);
-                    logger.info("Mutating  {}", getClass().getName() + " Mutate " + ctMethod.getName() + " on " +ctMethod.getDeclaringClass().getName() + " of " +targetProject.getLocation());
-                    Utils.write(ctMethod.getDeclaringClass(), this.targetProject.getClassesLocation());
-                    Report report = new Report(MutantState.STARTED, getClass().getName() + " Mutate " + ctMethod.getName() + " on class " + ctMethod.getDeclaringClass().getName());
-                    InvocationResult testResult = testRunner.run();
-                    if(testResult.getExitCode() != 0){
-                        report.setMutantState(MutantState.KILLED);
-                    }
-                    else{
-                        report.setMutantState(MutantState.SURVIVED);
-                    }
+				if (!m.isEmpty()) {
+					iterator.writeByte(m.get(pos), pos);
+					logger.info("Mutating  {}", getClass().getName() + " Mutate " + ctMethod.getName() + " on "
+							+ ctMethod.getDeclaringClass().getName() + " of " + targetProject.getLocation());
+					Utils.write(ctMethod.getDeclaringClass(), this.targetProject.getClassesLocation());
+					Report report = new Report(MutantState.STARTED, getClass().getName() + " Mutate "
+							+ ctMethod.getName() + " on class " + ctMethod.getDeclaringClass().getName());
+					InvocationResult testResult = testRunner.run();
+					if (testResult.getExitCode() != 0) {
+						report.setMutantState(MutantState.KILLED);
+					} else {
+						report.setMutantState(MutantState.SURVIVED);
+					}
 
-                    if(ReportService.getInstance().getReports().get(this) == null){
-                        List<Report> mutantReportList = new ArrayList<>();
-                        mutantReportList.add(report);
-                        ReportService.getInstance().getReports().put(this, mutantReportList);
-                    }
-                    else{
-                        ReportService.getInstance().getReports().get(this).add(report);
-                    }
+					if (ReportService.getInstance().getReports().get(this) == null) {
+						List<Report> mutantReportList = new ArrayList<>();
+						mutantReportList.add(report);
+						ReportService.getInstance().getReports().put(this, mutantReportList);
+					} else {
+						ReportService.getInstance().getReports().get(this).add(report);
+					}
 
-                    this.revert();
-                }
+					this.revert();
+				}
 			}
 
 		}
@@ -132,7 +113,8 @@ public class ComparisonOperatorMutator implements Mutator {
 	@Override
 	public void revert() throws CannotCompileException, IOException, BadBytecode {
 
-		logger.info("Reverting  {}", getClass().getName() + "Revert " + modified.getName() + " on " + targetProject.getLocation());
+		logger.info("Reverting  {}",
+				getClass().getName() + "Revert " + modified.getName() + " on " + targetProject.getLocation());
 		modified.getDeclaringClass().defrost();
 		modified.setBody(original, null);
 		Utils.write(modified.getDeclaringClass(), this.targetProject.getClassesLocation());
--- mutator/src/test/java/fr/istic/m2il/vv/mutator/loader/JavaAssistHelperTest.java
+++ mutator/src/test/java/fr/istic/m2il/vv/mutator/loader/JavaAssistHelperTest.java
@@ -1,26 +1,73 @@
 package fr.istic.m2il.vv.mutator.loader;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.istic.m2il.vv.mutator.common.CheckConfigurtionProperties;
+import fr.istic.m2il.vv.mutator.common.TimeWatch;
+import fr.istic.m2il.vv.mutator.config.ConfigOption;
+import fr.istic.m2il.vv.mutator.targetproject.TargetProject;
+import javassist.ClassPool;
+import javassist.Loader;
+
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
 public class JavaAssistHelperTest {
-    @Before
-    public void setUp() throws Exception {
+
+	ClassPool pool;
+	Loader loader;
+	CustomTranslator translator;
+	TargetProject targetProject;
+	List<Class<?>> classes;
+	List<Class<?>> tests;
+	File fileMocked;
+	JavaAssistHelper javaHelp;
+
+	@Before
+	public void setUp() throws Exception {
+		pool = new ClassPool();
+		loader = new Loader();
+		//Class<Loader> cl = mock(Class.class);
+		translator = new CustomTranslator();
+		javaHelp = JavaAssistHelper.getInstance();
+		targetProject = TargetProject.getInstance();
+		classes = new ArrayList<>();
+		tests = new ArrayList<>();
+        classes.add(ConfigOption.class);
+        tests.add(TimeWatch.class);
+        targetProject.setClasses(classes);
+        targetProject.setTests(tests);
+        //fileMocked = mock(File.class);
+        targetProject.setLocation(new File("../input"));
+        targetProject.setClassesLocation(new File("../input/target/classes"));
+        targetProject.setTestsLocation(new File("../input/target/test-classes"));
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@After
+	public void tearDown() throws Exception {
+	}
 
-    @Test
-    public void getInstance() throws Exception {
-    }
+	@Test
+	public void getInstance() throws Exception {
+	}
 
-    @Test
-    public void getPool() throws Exception {
-    }
+	@Test
+	public void getPool() throws Exception {
+	}
+
+	@Test
+	public void initPool() throws Exception {
+
+		Assert.assertNotNull(javaHelp.getPool().get("fr.istic.m2il.vv.input.Addition"));
+	}
 
 }
